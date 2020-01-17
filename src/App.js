605009c556592ad3ac38a0230f552c8ed8670245
azhu98 <43658543+azhu98@users.noreply.github.com>
2020-01-17 08:58:31 -0800

    new commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+    <header> 
+      <img src="https://www.freeiconspng.com/uploads/message-icon-png-0.png"/>
+      Chatter
+    </header>
+  
+  <div className="chatter-bubble">
+    Hello!~ This is Chatterbot.
+
+  </div>
+  <div className="speech-bubble">
+    Hi
+
+  </div>
+  <div className="speech-bubble2">
+    Nice to meet you
+
+  </div>
+  <div className="speech-bubble3">
+    My name is Amanda 😊
+
+  </div>
+    <TextInput onSend={t=> console.log(t)} />
+    
+  </main>
+}
+
+function TextInput(props) {
+  const [text, setText] = useState('')
+
+  return <div className="text-input">
+    <input className="text-box" value={text}
+      placeholder="  Write your message..."
+      onChange={e=> setText(e.target.value)}
+    />
+    <button className="send-button" onClick={()=> {
+      if(props.onSend) props.onSend(text)
+      setText('')
+    }}>
+      <img src="https://pngriver.com/wp-content/uploads/2017/12/download-Email-symbol-PNG-transparent-images-transparent-backgrounds-PNGRIVER-COM-4b8266_f0ef03475d104c6db271009986bdad0fmv2_d_1201_1201_s_2.png" />
+    </button>
+  </div> 
 }
 
 export default App;
